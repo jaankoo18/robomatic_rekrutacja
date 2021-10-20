34ecde3f59b0dd044e4f044879e70a1dd70c5bd8
Jan Krzysztof Sosulski <jan.sosulski.stud@pw.edu.pl>
2021-10-20 23:10:30 +0200

    zad4 v1.1.0

--- zad4/find_bigest_blue_element.py
+++ zad4/find_bigest_blue_element.py
@@ -1,37 +1,46 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("circles.png")
+def find_biggest_blue_element(file_name):
+    img = cv2.imread(str(file_name))
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-lower_blue = np.array([110, 50, 50])
-upper_blue = np.array([130, 255, 255])
+    lower_blue = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
 
-mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-#finding the biggest area
-if len(contours) >= 1:
-    biggest_area = cv2.contourArea(contours[0])
-    contour_to_draw = contours[0]
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        print(area)
-        if area > biggest_area:
-            biggest_area = area
-            contour_to_draw = contour
-else:
-    print("nie ma niebieskiego obiektu")
+    # finding the biggest area
+    if len(contours) >= 1:
+        biggest_area = cv2.contourArea(contours[0])
+        contour_to_draw = contours[0]
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            print(area)
+            if area > biggest_area:
+                biggest_area = area
+                contour_to_draw = contour
+    else:
+        raise MyError("There is no blue element!")
 
-cv2.drawContours(img, contour_to_draw, -1, (0, 0, 0), 3)
-#cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
-cv2.imshow("lel", img)
-cv2.imshow("lell", mask)
+    cv2.drawContours(img, contour_to_draw, -1, (0, 255, 0), 2)
+    #cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow("image", cv2.resize(img, (960, 540)))
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+class MyError(Exception):
+    def __init__(self, p1):
+        self.parametr1 = p1
+
+    def __str__(self):
+        return "Exception: " + self.parametr1
+
 
 
---
+++ zad4/zad4_test.py
@@ -0,0 +1,17 @@
+import sys
+from find_bigest_blue_element import find_biggest_blue_element
+
+#parametrem wywołania powinna być ścieżka do zdjęcia
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) >= 2:
+            find_biggest_blue_element(sys.argv[1])
+
+    except Exception as err:
+        print(err)
+
+    else:
+        print("The program ran without exceptions!")
+
+    finally:
+        print("The program ended correctly!")
